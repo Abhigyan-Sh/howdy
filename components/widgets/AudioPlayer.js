340abcb06669dbbf2e259cb97e533eb4197e54c3
pryansh <pryanshukla0321@gmail.com>
2024-03-03 02:06:37 +0530

    react-audio-gear :fire: for audioPlayback

--- components/widgets/AudioPlayer.js
+++ components/widgets/AudioPlayer.js
@@ -1,7 +1,8 @@
+import { AudioGear } from 'react-audio-gear'
 import { CloudinaryVideo } from '@cloudinary/url-gen'
 import { bitRate } from '@cloudinary/url-gen/actions/transcode'
 import { extractCloudinaryPublicId } from '../../utils/extractCloudinaryPublicId'
-import { getFileFormat } from '../../utils/computeFileProps'
+// import { getFileFormat } from '../../utils/computeFileProps'
 
 const AudioPlayer = ({ src }) => {
   /* bitRateValue: this factor can be manipulated by taking into 
@@ -9,15 +10,18 @@ const AudioPlayer = ({ src }) => {
   const bitRateValue = '90k'
   const publicId = new CloudinaryVideo(extractCloudinaryPublicId(src))
     .transcode(bitRate(bitRateValue)).publicID
+  
   return (
-    <audio controls>
-      <source 
-        // src={src} 
-        // exposing the audio directory path
-        src={`https://res.cloudinary.com/dfgh07xa9/video/upload/${publicId}`} 
-        type={`audio/${getFileFormat(src)}`} />
-      Your browser does not support the audio element.
-    </audio>
+    <AudioGear 
+      audioSrc={`https://res.cloudinary.com/dfgh07xa9/video/upload/${publicId}`} />
+    // <audio controls>
+    //   <source 
+    //     // src={src} 
+    //     // exposing the audio directory path
+    //     src={`https://res.cloudinary.com/dfgh07xa9/video/upload/${publicId}`} 
+    //     type={`audio/${getFileFormat(src)}`} />
+    //   Your browser does not support the audio element.
+    // </audio>
   )
 }
 
